b49a6a9e375ba5a5625260101c28cb24a629ad6c
Vinu <kevinduss1@gmail.com>
2023-10-06 08:24:28 +0000

    move image from source to target container

--- function_app.py
+++ function_app.py
@@ -3,9 +3,12 @@ import logging
 
 app = func.FunctionApp()
 
-@app.blob_trigger(arg_name="myblob", path="card-high-res-images/{name}",
-                               connection="AzureWebJobsStorage") 
-def image_compressor(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob"
-                f"Name: {myblob.name}"
-                f"Blob Size: {myblob.length} bytes")
+@app.blob_trigger(arg_name="inputblob",
+                  path="techcamp-source/{name}",
+                  connection="AzureWebJobsStorage")
+@app.blob_output(arg_name="outputblob",
+                 path="techcamp-target-low/{name}",
+                 connection="AzureWebJobsStorage")
+def image_compressor(inputblob: func.InputStream, outputblob: func.Out[str]):
+    logging.info(f"Python blob trigger function processed blob with name: {inputblob.name}")
+    outputblob.set(inputblob.read())
